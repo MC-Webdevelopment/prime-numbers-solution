3db1b2d6e76261d3c27d2abac5834e0d660b7042
Salah Al-Dhaferi <binaryminded@gmail.com>
2021-03-28 22:40:40 +0300

    Added description

---
+++ README.md
@@ -0,0 +1,15 @@
+# Solution to the prime numbers lab
+
+This is one potential solution to the prime numbers lab. It builds upon the fact that all non-prime numbers (composite numbers) are built from (2,3,5, and 7). 
+
+## Examples: 
+- 9 (not prime) = 3 * 3
+- 10 (not prime) = 5 * 2
+- 11 (prime) = no two numbers can make this number
+- 12 (not prime) = 2 * 2 * 3
+- 13 (prime) = no two numbers can make this number
+- 14 (not prime) = 2 * 7
+- etc..
+
+
+Test it out! Try different numbers :D 
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,12 +1,29 @@
 function findPrimeNumbers(number) {
+  // If the number is 2, 3, 5, or 7 then its a prime number right away.
   if (number == 2 || number == 3 || number == 5 || number == 7) {
+    // log a message that says this is a prime number
     console.log(number + " is a prime number");
+    /* Since we already established the fact that the passed number is a prime number then there's no need to run 
+    the whole function. Therefore, we use an empty return keyword so that we can stop running the function. 
+    */
     return;
   }
 
-  if (number % 2 !== 0 && number % 3 !== 0 && number % 5 !== 0 &&number % 7 !== 0) {
+  /* If we reach this if statement then we can be sure that the passed number is not 2, 3, 5, or 7. Which means that 
+  in order for us to know if the passed number is a prime number or not we want to be sure that 
+  it's NOT divisible by 2, 3, 5, or 7 
+  */
+  if (number % 2 !== 0 && number % 3 !== 0 && number % 5 !== 0 && number % 7 !== 0) {
+    // log a message that says this is a prime number
     console.log(number + " is a prime number");
-  } else console.log(number + " is NOT a prime number");
+  } 
+  else {
+    // If we reach this else statement then just log that this is not a prime number
+    console.log(number + " is a prime number");
+  }
 }
 
+
+// Calling the function to test it. Go to your console in your browser to see the result. 
+// Change the number to see what happens :D
 findPrimeNumbers(89)
